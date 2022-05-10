ca89ffa917b937eee3a5737cb341203d893e45d5
En3Tho <razerael@gmail.com>
2022-05-10 14:55:32 +0300

    better subcommands view

--- PoshDotnetDumpAnalyzeViewer/SubcommandsView.cs
+++ PoshDotnetDumpAnalyzeViewer/SubcommandsView.cs
@@ -34,30 +34,59 @@ public interface IHelpCommand
 
 public static class SubcommandsView
 {
-    public static ListView ForOutputLine<T>(IOutputLine<T> line)
+    public static Dialog ForOutputLine<T>(IOutputLine<T> line, IClipboard clipboard, CommandQueue queue)
     {
-        var view = new ListView(); // to popup
+        // width is max commands width + padding
+        // height is commands count + padding
+
+        var dialog = new Dialog("Available commands"); // to popup
+
+        Button MakeButton(string title, Action action)
+        {
+            var button = new Button(title);
+            button.Clicked += () =>
+            {
+                Application.RequestStop(dialog);
+                action();
+            };
+            return button;
+        }
+
+        var buttons = new List<(int, Button)>();
 
         if (line is IAddress address)
         {
-            // helpers ...
+            var data = address.Address.ToString();
+            buttons.Add((1, MakeButton("Copy address", () => clipboard.SetClipboardData(data))));
         }
 
         if (line is IMethodTable methodTable)
         {
-            // helpers ...
+            var data = methodTable.MethodTable.ToString();
+            buttons.Add((1, MakeButton("Copy method table", () => clipboard.SetClipboardData(data))));
         }
 
         if (line is ITypeName typeName)
         {
-            // helpers ...
+            var data = typeName.TypeName.ToString();
+            buttons.Add((1, MakeButton("Copy type name", () => clipboard.SetClipboardData(data))));
         }
 
         if (line is IThreadId threadId)
         {
-            // helpers ...
+            var data = threadId.TheadId.ToString();
+            buttons.Add((1, MakeButton("Copy thread id", () => clipboard.SetClipboardData(data))));
         }
 
-        return view;
+        if (buttons.Count > 0)
+        {
+            var width = buttons.MaxBy(values => values.Item2.Text).Item2.Text.Length;
+            var height = buttons.Count;
+
+            dialog.Width = width;
+            dialog.Height = height;
+        }
+
+        return dialog;
     }
 }
